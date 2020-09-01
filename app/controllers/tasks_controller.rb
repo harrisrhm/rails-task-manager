511985ee9e935f31d64a2ceca1c2eabc6754318a
Harris Rahim <harris.rhm@gmail.com>
2020-09-01 15:38:05 +1000

    task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        @tasks = Task.all
+    end
+
+    def show
+        @completed = completed?(@task)
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def create
+        @task = Task.new(task_params)
+        @task.save
+    end
+
+    def edit; end
+
+    def update
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+        redirect_to task_path(@task)
+    end
+
+    def destroy
+        @task.destroy
+        redirect_to tasks_path
+    end
+
+    private
+
+    def completed?(task)
+        task.completed ? "this task is completed" : "not completed yet"
+    end
+
+    def task_params
+        params.require(:task).permit(:title, :details, :completed)
+    end
+
+    def set_task
+        @task = Task.find(params[:id])
+    end
+
+end
